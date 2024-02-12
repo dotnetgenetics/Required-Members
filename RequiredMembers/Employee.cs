49f87a66f7d4ddc6537f8e5a9bc8228a01a1ad0e
Gregory Nelson Esguerra <gregory_keyboard@outlook.com>
2024-02-12 18:24:58 +0800

    Add project files.

---
+++ RequiredMembers/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace RequiredMembers
+{
+    public class Employee
+    {
+        public required int ID { get; set; } = 0;
+        public required string Name { get; set; } = string.Empty;
+        public required string Address { get; set; } = string.Empty;
+        public int Age { get; set; } = 0;
+
+
+        public Employee()
+        {
+            //default constructor        
+        }
+
+        [SetsRequiredMembers]
+        public Employee(int id, string name, string address)
+        {
+            ID = id;
+            Name = name;
+            Address = address;                
+        }
+    }
+}
---
+++ RequiredMembers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace RequiredMembers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void TestEmployee()
+        {
+            //set required members
+            var employee = new Employee { ID = 1001, Name = "Greg Esguerra", Address = "Cebu City" };
+            Employee employee1 = new Employee(1001, "Greg Esguerra", "Cebu City");
+        }
+    }
+}
